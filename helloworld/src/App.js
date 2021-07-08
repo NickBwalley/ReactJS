66e50bcf6c749d3a84cdafbb05ff0689855901ed
NickBwalley <nickbwalley@gmail.com>
2021-07-08 16:39:56 +0300

    ReactApp1

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Tweet from './Tweet';
+import "./App.css";
 
 function App(){
   // const sayHello = () =>{
   //   console.log("Hello Kenya!");
   // };
   return(
-    <div>
+    <div className="app">
       <h1>Hello React!</h1>            
+      <Tweet/>
+      <br/>
       <Tweet/>
     </div>
   )
