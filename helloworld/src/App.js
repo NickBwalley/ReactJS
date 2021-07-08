957da0088534ab810dc1b754aa2ba5cceacc8fca
NickBwalley <nickbwalley@gmail.com>
2021-07-08 16:54:16 +0300

    ReactApp1

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -8,10 +8,10 @@ function App(){
   // };
   return(
     <div className="app">
-      <Tweet name="David Blaine"/>
-      <Tweet name="Aaron Spears"/>
-      <Tweet name="Jay Cutler"/>
-      <Tweet name="Ronnie Coleman"/>
+      <Tweet name="David Blaine" message="I am a Great Magician"/>
+      <Tweet name="Aaron Spears" message="I am the best Drummist"/>
+      <Tweet name="Jay Cutler" message="Four Time Mr.Olympia"/>
+      <Tweet name="Ronnie Coleman" message="Eight Time Mr.Olympia"/>
       
     </div>
   )
