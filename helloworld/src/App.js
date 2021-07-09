fb80d0103795da38d26e9cc0477d05cd23622aac
NickBwalley <nickbwalley@gmail.com>
2021-07-09 16:13:11 +0300

    ReactJs

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import React, {useState} from 'react';
 import Tweet from './Tweet';
 import "./App.css";
 
@@ -6,15 +7,21 @@ function App(){
   // const sayHello = () =>{
   //   console.log("Hello Kenya!");
   // };
+    const [isRed, setRed] = useState(false);
+    const[count, setCount] = useState(0);
+    const increment = () =>{
+      setCount(count + 1);
+    };
   return(
     <div className="app">
       <Tweet name="David Blaine" message="I am a Great Magician"/>
       <Tweet name="Aaron Spears" message="I am the best Drummist"/>
       <Tweet name="Jay Cutler" message="Four Time Mr.Olympia"/>
       <Tweet name="Ronnie Coleman" message="Eight Time Mr.Olympia"/>
-      
+      <button onClick={increment}>Increment </button>
+      <h1>{count}</h1>
     </div>
-  )
+  );
 }
 
 export default App;
