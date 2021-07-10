7566342a14bfbf180c44bf7488edf4332755cd1c
NickBwalley <nickbwalley@gmail.com>
2021-07-10 21:23:24 +0300

    ReactJS

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import React, {useState} from 'react';
+import React, {useState} from "react";
 import Tweet from './Tweet';
-import "./App.css";
+
 
 function App(){
   // const sayHello = () =>{
@@ -14,10 +13,7 @@ function App(){
     };
   return(
     <div className="app">
-      <Tweet name="David Blaine" message="I am a Great Magician"/>
-      <Tweet name="Aaron Spears" message="I am the best Drummist"/>
-      <Tweet name="Jay Cutler" message="Four Time Mr.Olympia"/>
-      <Tweet name="Ronnie Coleman" message="Eight Time Mr.Olympia"/>
+
       <button onClick={increment}>Increment </button>
       <h1>{count}</h1>
     </div>
