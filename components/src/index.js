53c8a7e26e9b5331a70bec1ef1351fa27dfe7b91
NickBwalley <nickbwalley@gmail.com>
2021-09-13 16:57:02 +0300

    React-JS components.

--- components/src/index.js
+++ components/src/index.js
@@ -8,7 +8,24 @@ if(module.hot){
 };
 
 const App = () => {
-    return <div>Hey there!</div>
+    return(
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>            
+        </div>
+    );
 };
 
 // render the React Application to the screen
