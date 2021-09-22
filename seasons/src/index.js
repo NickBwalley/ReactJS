1659a204a43c6afb74270cd811cb8650d5aa215f
NickBwalley <nickbwalley@gmail.com>
2021-09-22 11:14:47 +0300

    react using setState to find latitude

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -6,15 +6,24 @@ class App extends React.Component {
   // constructor
   constructor(props) {
     super(props); // is a reference to the parents constructor function
-    this.state = { lat: 40 };
+
+    // THIS IS THE ONLY TIME we do direct assignment to this.state
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // we called setState!!!
+        this.setState({ lat: position.coords.latitude });
+
+        // we DID NOT!!!
+        // this.state.lat = position.coords.latitude;
+      },
+      (err) => console.log(err)
+    );
   }
 
   // React says we have to define render()
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
     return <div>Latitude: {this.state.lat}</div>;
   }
 }
