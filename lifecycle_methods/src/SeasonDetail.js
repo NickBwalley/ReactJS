b18e52fabf0e56036127b6591b7ad80a290e7436
NickBwalley <nickbwalley@gmail.com>
2021-09-23 22:53:50 +0300

    ternary expression in jsx

--- lifecycle_methods/src/SeasonDetail.js
+++ lifecycle_methods/src/SeasonDetail.js
@@ -8,10 +8,17 @@ const getSeason = (lat, month) => {
   }
 };
 const SeasonDetail = (props) => {
-  console.log(props.lat);
   const season = getSeason(props.lat, new Date().getMonth());
-  console.log(season);
-  return <div>Season Display</div>;
+  const text =
+    season === "winter" ? "Burr!, its chilly!" : "Lets hit the beach buddy!";
+  const icon = season === "winter" ? "snowflake" : "sun";
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDetail;
