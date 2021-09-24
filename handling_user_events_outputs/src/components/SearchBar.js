a23235f6d413c56ad11446d4e7a59b8f9817e651
NickBwalley <nickbwalley@gmail.com>
2021-09-24 20:39:00 +0300

    alternate user event handler

--- handling_user_events_outputs/src/components/SearchBar.js
+++ handling_user_events_outputs/src/components/SearchBar.js
@@ -1,13 +1,17 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="ui field">
             <label>Image search</label>
-            <input type="text" />
+            <input type="text" onChange={this.onInputChange} />
+            {/* onChange is a callback function */}
           </div>
         </form>
       </div>
@@ -16,3 +20,8 @@ class SearchBar extends React.Component {
 }
 
 export default SearchBar;
+
+/* 
+1. Arrow Function
+onChange={(event) => console.log(event.target.value)} 
+2. callback function (instead of using () you just leave it inside the other function.)*/
