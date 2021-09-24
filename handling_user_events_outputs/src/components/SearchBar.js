f8d806a282798f89545b4decbab86c42622e8390
NickBwalley <nickbwalley@gmail.com>
2021-09-24 20:47:08 +0300

    uncontrolled and controlled elements

--- handling_user_events_outputs/src/components/SearchBar.js
+++ handling_user_events_outputs/src/components/SearchBar.js
@@ -1,16 +1,19 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  state = { term: "" };
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="ui field">
             <label>Image search</label>
-            <input type="text" onChange={this.onInputChange} />
+            <input
+              value={this.state.term}
+              type="text"
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
             {/* onChange is a callback function */}
           </div>
         </form>
