a42f5d444c5c934c5291acefbf8289c2dae72144
NickBwalley <nickbwalley@gmail.com>
2021-09-29 12:59:40 +0300

    get videos from youtube apis

--- youtube_videos/src/components/App.js
+++ youtube_videos/src/components/App.js
@@ -2,17 +2,22 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import Youtube from "../apis/Youtube";
 class App extends React.Component {
-  onTermSubmit = (term) => {
-    Youtube.get("/search", {
+  state = { videos: [] };
+  onTermSubmit = async (term) => {
+    const response = await Youtube.get("/search", {
       params: {
         q: term,
       },
     });
+
+    this.setState({ videos: response.data.items });
   };
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <SearchBar onFormSubmit={this.onTermSubmit} />I have{" "}
+        {this.state.videos.length} videos.
       </div>
     );
   }
