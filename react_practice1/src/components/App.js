8c3be2480d89735bbf0e49349d4e8c2f9ba163fc
NickBwalley <nickbwalley@gmail.com>
2021-09-30 23:58:16 +0300

    react practice

--- react_practice1/src/components/App.js
+++ react_practice1/src/components/App.js
@@ -1,13 +1,30 @@
 import React from "react";
+import axios from "axios";
 import PasswordField from "./PasswordField";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await axios.get(
+      "https://api.unsplash.com//search/photos",
+      {
+        params: { query: term },
+        headers: {
+          Authorization:
+            "Client-ID DCJojAQKTzchib0BiOAvx-o79ufwyGT3F_MsLcDJqPo",
+        },
+      }
+    );
+    this.setState({ images: response.data.results });
+  };
+
   render() {
     return (
-      <div>
-        <SearchBar />
-        <PasswordField />
+      <div className="ui container" style={{ margin: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        Found {this.state.images.length} images.
       </div>
     );
   }
