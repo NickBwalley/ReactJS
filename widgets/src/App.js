76365c0f7c8a6a06a031a2e8b5e5c459673a5c8a
NickBwalley <nickbwalley@gmail.com>
2021-10-01 11:34:47 +0300

    introducing useState

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,5 +1,25 @@
 import React from "react";
+import Accordion from "./components/Accordion";
 
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a frontend JavaScript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among Engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
 export default () => {
-  return <h1>Widgets App</h1>;
+  return (
+    <div>
+      <br />
+      <Accordion items={items} />
+    </div>
+  );
 };
