558a9608194419775a80395746b0a995901e6ed7
NickBwalley <nickbwalley@gmail.com>
2021-10-01 16:06:50 +0300

    rendering results from wikipedia api using function component

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Accordion from "./components/Accordion";
-import ClickToCount from "./components/ClickToCount";
+// import Accordion from "./components/Accordion";
+// import ClickToCount from "./components/ClickToCount";
 import Search from "./components/Search";
 
 const items = [
--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,8 +1,39 @@
-import React from "react";
-const useState = React.useState;
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+    if (term) {
+      search();
+    }
+  }, [term]); // this term renders at initial data loading and every time it changes
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          {result.snippet}
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -16,6 +47,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
