1e0cc7028b300020a7d7e0c3f186f41834a704c3
NickBwalley <nickbwalley@gmail.com>
2021-10-05 09:00:22 +0300

    adding the language input

--- widgets/src/components/Translate.js
+++ widgets/src/components/Translate.js
@@ -17,10 +17,23 @@ const options = [
 ];
 const Translate = () => {
   const [language, setLanguage] = useState(options[0]);
-
+  const [text, setText] = useState("");
   return (
     <div>
+      <div className="ui form">
+        <div className="ui field">
+          <label>Enter Text</label>
+          <input
+            value={text}
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+          />
+        </div>
+      </div>
+
       <Dropdown
+        label="Select a Language"
         options={options}
         onSelectedChange={setLanguage}
         selected={language}
