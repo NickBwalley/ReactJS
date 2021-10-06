31eb7522af43f797d06d6e7ca064d6a42d5672a2
NickBwalley <nickbwalley@gmail.com>
2021-10-06 08:59:45 +0300

    building a reusable route component

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -4,6 +4,7 @@ import ClickToCount from "./components/ClickToCount";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
 
 const items = [
   {
@@ -38,11 +39,52 @@ export default () => {
   const [selected, setSelected] = useState(options[0]);
   const [showDropDown, setShowDropDown] = useState(true);
 
-  const showAccordion = () => {
-    if (window.location.pathname === "/") {
-      return <Accordion items={items} />;
-    }
-  };
+  // const showAccordion = () => {
+  //   if (window.location.pathname === "/") {
+  //     return <Accordion items={items} />;
+  //   }
+  // };
 
-  return <div>{showAccordion()}</div>;
+  // const showList = () => {
+  //   if (window.location.pathname === "/list") {
+  //     return <Search />;
+  //   }
+  // };
+
+  // const showDropdown = () => {
+  //   if (window.location.pathname === "/dropdown") {
+  //     return <Dropdown />;
+  //   }
+  // };
+
+  // const showTranslate = () => {
+  //   if (window.location.pathname === "/translate") {
+  //     return <Translate />;
+  //   }
+  // };
+
+  return (
+    <div>
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+
+      <Route path="/list">
+        <Search />
+      </Route>
+
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a Color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+
+      <Route path="/translate">
+        <Translate />
+      </Route>
+    </div>
+  );
 };
