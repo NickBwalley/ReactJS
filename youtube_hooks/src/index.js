2b60e9fb71383270c07ac9c632da076b26aa0659
NickBwalley <nickbwalley@gmail.com>
2021-10-06 13:56:16 +0300

    deployment with vercel

--- youtube_hooks/src/index.js
+++ youtube_hooks/src/index.js
@@ -3,3 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
+
+// deployment to vercel: https://youtube-hooks-rouge.vercel.app
+/**
+ * sudo npm install -g vercel
+ * vercel login
+ * vercel
+ * vercel --prod
+ */
