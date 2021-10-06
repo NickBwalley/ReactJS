37ce54f2f2f5a176565fb1f6751d89f492e3a515
NickBwalley <nickbwalley@gmail.com>
2021-10-06 09:13:08 +0300

    implementing a header for navigation

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -5,6 +5,7 @@ import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
 import Route from "./components/Route";
+import Header from "./components/Header";
 
 const items = [
   {
@@ -39,6 +40,32 @@ export default () => {
   const [selected, setSelected] = useState(options[0]);
   const [showDropDown, setShowDropDown] = useState(true);
 
+  return (
+    <div>
+      <Header />
+
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+
+      <Route path="/list">
+        <Search />
+      </Route>
+
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a Color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+
+      <Route path="/translate">
+        <Translate />
+      </Route>
+    </div>
+  );
   // const showAccordion = () => {
   //   if (window.location.pathname === "/") {
   //     return <Accordion items={items} />;
@@ -62,29 +89,4 @@ export default () => {
   //     return <Translate />;
   //   }
   // };
-
-  return (
-    <div>
-      <Route path="/">
-        <Accordion items={items} />
-      </Route>
-
-      <Route path="/list">
-        <Search />
-      </Route>
-
-      <Route path="/dropdown">
-        <Dropdown
-          label="Select a Color"
-          options={options}
-          selected={selected}
-          onSelectedChange={setSelected}
-        />
-      </Route>
-
-      <Route path="/translate">
-        <Translate />
-      </Route>
-    </div>
-  );
 };
