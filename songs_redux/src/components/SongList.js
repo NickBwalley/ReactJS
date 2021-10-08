2549f4671eeea67384b485d1e34a9e718aa0b35a
NickBwalley <nickbwalley@gmail.com>
2021-10-08 10:50:25 +0300

    the connct function

---
+++ songs_redux/src/components/SongList.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class SongList extends Component {
+  render() {
+    return <div>SongList</div>;
+  }
+}
+
+export default connect()(SongList);
+
+/*
+function connect() {
+    return function(){
+        return 'hi there!'
+    }
+}
+
+function()();
+
+*/
