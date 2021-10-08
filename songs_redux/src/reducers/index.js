553cf098fd92ba1a1a600fb62e21d949a07bbf6e
NickBwalley <nickbwalley@gmail.com>
2021-10-08 10:30:11 +0300

    building reducers

---
+++ songs_redux/src/reducers/index.js
@@ -0,0 +1,16 @@
+const songReducer = () => {
+  return [
+    { title: "My land is Kenya", duration: "4:05" },
+    { title: "Yummy", duration: "3:15" },
+    { title: "South of the Border", duration: "4:30" },
+    { title: "1000 Nights", duration: "2:45" },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
