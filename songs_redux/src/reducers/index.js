b056525fe471454cc7f1858c6eaddbc17c733071
NickBwalley <nickbwalley@gmail.com>
2021-10-08 10:37:16 +0300

    wiring up the provider

--- songs_redux/src/reducers/index.js
+++ songs_redux/src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from "redux";
+
 const songReducer = () => {
   return [
     { title: "My land is Kenya", duration: "4:05" },
@@ -14,3 +16,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
 
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songReducer,
+  selectedSong: selectedSongReducer,
+});
