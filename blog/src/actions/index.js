aa1615389ba47907d6746da517336c3c52591e76
NickBwalley <nickbwalley@gmail.com>
2021-10-12 10:23:41 +0300

    async and await in redux world

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,10 @@
-export const fetchPosts = () => {
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPosts = async () => {
+  // bad approach!!!
+  const response = await jsonPlaceholder.get("/post");
   return {
     type: "FETCH_POSTS",
+    payload: response,
   };
 };
