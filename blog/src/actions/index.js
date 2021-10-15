efbec30f176595103b7eab5f33f1e86093071b7e
NickBwalley <nickbwalley@gmail.com>
2021-10-15 21:51:52 +0300

    fincing unique userIds

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,10 +1,11 @@
-import jsonPlaceholder from "../apis/jsonPlaceholder";
 import _ from "lodash";
+import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPostsAndUsers = () => async (dispatch) => {
-  console.log("about to fetch posts");
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
-  console.log("post already fetched!");
+
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach((id) => dispatch(fetchUser(id)));
 };
 
 export const fetchPosts = () => async (dispatch) => {
