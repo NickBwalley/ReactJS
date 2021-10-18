098755b942fe9c6555656f25d20de868164cfbe1
NickBwalley <nickbwalley@gmail.com>
2021-10-18 10:11:34 +0300

    introduction to react router

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const PageOne = () => {
+  return (
+    <div>
+      PageOne
+      <button>Click me!</button>
+    </div>
+  );
+};
+
+const PageTwo = () => {
+  return <div> PageTwo</div>;
+};
 
 const App = function () {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
