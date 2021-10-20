ac0c9b2549e7736178c2b5818abbbec894d37b49
NickBwalley <nickbwalley@gmail.com>
2021-10-20 12:55:07 +0300

    creating a stream with REST conventions

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
 import { connect } from "react-redux";
-import { createStream } from "../../actions/";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -25,10 +25,9 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit(formValues) {
-    // event.preventDefault();
-    console.log(formValues);
-  }
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
+  };
 
   render() {
     return (
@@ -62,7 +61,9 @@ const validate = (formValues) => {
   return errors;
 };
 
-export default reduxForm({
+const wrappedForm = reduxForm({
   form: "streamCreate",
   validate,
 })(StreamCreate);
+
+export default connect(null, { createStream })(wrappedForm);
