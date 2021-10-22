abf883f10142e86b3d73cd4fa7137d850f19b8d0
NickBwalley <nickbwalley@gmail.com>
2021-10-22 13:06:07 +0300

    edit form submission

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -45,13 +45,14 @@ export const fetchStream = (id) => async (dispatch) => {
 };
 
 export const editStream = (id, formValues) => async (dispatch) => {
-  const response = await streams.put(`/streams${id}`, formValues);
+  const response = await streams.put(`/streams/${id}`, formValues);
 
   dispatch({ type: EDIT_STREAM, payload: response.data });
+  history.push("/");
 };
 
 export const deleteStream = (id) => async (dispatch) => {
-  await streams.delete(`streams/${id}`);
+  await streams.delete(`/streams/${id}`);
 
   dispatch({ type: DELETE_STREAM, payload: id });
 };
