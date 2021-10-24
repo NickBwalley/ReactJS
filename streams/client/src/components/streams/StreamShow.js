4164759421754a9a17a8f27ef1d2d33c9b9a6b10
NickBwalley <nickbwalley@gmail.com>
2021-10-24 09:33:21 +0300

    optional player building

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -6,17 +6,32 @@ import { fetchStream } from "../../actions";
 class StreamShow extends React.Component {
   constructor(props) {
     super(props);
+
     this.videoRef = React.createRef();
   }
+
   componentDidMount() {
     const { id } = this.props.match.params;
+
     this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    const { id } = this.props.match.params;
     this.player = flv.createPlayer({
       type: "flv",
-      url: "https://localhost:8000/live/${id}.flv",
+      url: `http://localhost:8000/live/${id}.flv`,
     });
-
-    this.player.attachMediaElement(ths.videoRef.current);
+    this.player.attachMediaElement(this.videoRef.current);
     this.player.load();
   }
 
@@ -29,7 +44,7 @@ class StreamShow extends React.Component {
 
     return (
       <div>
-        <video ref={this.videoRef} style={{ width: "100%" }} controls={true} />
+        <video ref={this.videoRef} style={{ width: "100%" }} controls />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
